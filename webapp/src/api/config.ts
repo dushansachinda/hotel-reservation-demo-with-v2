14ad223bf22b84b05e97f5110888c992af69bea0
dushansachinda <dushanusa81@gmail.com>
2024-04-25 22:36:09 -0700

    graphQL BfF catalog service invocation

--- webapp/src/api/config.ts
+++ webapp/src/api/config.ts
@@ -2,8 +2,10 @@ declare global {
   interface Window {
     configs: {
       apiUrl: string;
+      catalogUrl: string;
     };
   }
 }
 
 export const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "http://localhost:3000/api/reservations";
+export const catalogUrl = window?.configs?.catalogUrl ? window.configs.catalogUrl : "http://localhost:9090";
